4a2b0bd8f0e4e7e39ac4470b7ef89ce684d6e968
Linse5420 <linse.paulmathew@student.sl.on.ca>
2022-06-16 16:09:13 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import requests from './request';
 import './App.css';
+import Row from './Row';
+import Banner from './Banner';
+import { Nav } from './Nav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Nav/>
+    <Banner/>
+    
+       <Row title="NETFLIX ORGINALS" 
+       fetchUrl={requests.fetchNetflixOrginals}
+       isLargeRow={true}
+       />
+       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+       <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+       <Row title="Action Packed" fetchUrl={requests.fetchActionMovies}/>
+       <Row title="Comedy movies" fetchUrl={requests.fetchComedyMovies}/>
+       <Row title="Horror" fetchUrl={requests.fetchHorrorMovies}/>
+       <Row title="Romance" fetchUrl={requests.fetchRomanceMovies}/>
+       <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Nav.css';
+export const Nav = () => {
+    
+  return (
+    <div className="nav" >
+
+    <img className='nav_logo'
+    src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1200px-Netflix_2015_logo.svg.png"
+    alt="logo"/>
+    <img className='nav_avatar'
+    src="https://bestprofilepictures.com/wp-content/uploads/2020/07/Cool-Profile-Picture-For-Instagram-1003x1024.jpg"
+    alt="avatar"/>
+    
+    
+    </div>
+  )
+}
+export default Nav;
